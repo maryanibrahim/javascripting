2898de2608e5edb2366c02b2156b3d9e92ed2e66
Maryan ibrahim <53503272+maryanibrahim@users.noreply.github.com>
2023-05-05 20:30:38 -0500

    function

---
+++ function.js
@@ -0,0 +1,7 @@
+// Define the `eat` function
+function eat(food) {
+  return food + ' tasted really good.';
+}
+
+// Call the `eat` function with the string 'bananas' as the argument
+console.log(eat('bananas'));
