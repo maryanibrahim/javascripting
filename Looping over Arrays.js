9fa1cd8d92ae0d91f66932820b126ae70d7d3651
Maryan ibrahim <53503272+maryanibrahim@users.noreply.github.com>
2023-05-05 19:13:49 -0500

    Looping

---
+++ Looping over Arrays.js
@@ -0,0 +1,14 @@
+//Traditional for Loops Can Work
+const amounts = [61.00, 52.25, 112.99, 5.00]; 
+let total = 0;
+for (let i = 0; i < amounts.length; i++) {
+  total += amounts[i];
+}
+console.log('Order total is: ', total);
+
+//Newer for..of Loops Are Simpler
+let total = 0;
+for (let amount of amounts) {
+  total += amount;
+}
+console.log('Order total is: ', total);
